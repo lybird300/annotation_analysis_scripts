4a619f9ca71d46fc34c0377affbc10474566a02b
Nicole Ruiz <nicole.ruiz01@gmail.com>
2016-04-29 22:27:36 -0600

    Add command to get all variants with multiple alternate alleles

--- variant_analysis.sh
+++ variant_analysis.sh
@@ -2,3 +2,6 @@
 
 # Checking annovar_refGene.txt to see what kind of transcript IDs are in it (NM_, NR_, etc)
 awk '$i=substr($i,1,3)' annovar_refGene.txt | sort | uniq -c
+
+# Find all variants with more than 1 alternate allele
+grep -v "^#" SeattleSeqAnnotation138.NA12878_Y_removed.vcf.359966521039.txt | awk '{print $5}' | awk '$0 ~ /,/ {print $0}' | less -S
